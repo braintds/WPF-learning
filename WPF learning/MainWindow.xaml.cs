fbbf7804d53bd45eefb1c81841a7ec56a2fd9479
Kirill <freestyle.ff50@gmail.com>
2023-02-22 03:44:59 +0700

    Добавьте файлы проекта.

---
+++ WPF learning/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_learning.Models;
+using WPF_learning.Services;
+
+namespace WPF_learning
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoModel> TodoDataList;
+        private FileIOService FileIOService;
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.FileIOService = new FileIOService(PATH);
+
+            try
+            {
+                this.TodoDataList = FileIOService.LoadData();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                this.Close();
+            }
+
+
+            this.dgTodoList.ItemsSource = TodoDataList;
+            this.TodoDataList.ListChanged += TodoDataList_ListChanged;
+        }
+
+        private void TodoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    FileIOService.SaveData(sender);
+                }
+                catch (Exception exception)
+                {
+
+                    MessageBox.Show(exception.Message);
+                    this.Close();
+
+                }
+            }
+        }
+    }
---
+++ WPF learning/Models/TodoModel.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_learning.Models
+{
+    class TodoModel : INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        private bool IsDone;
+        private string Text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool isDone
+        {
+            get { return this.IsDone; }
+            set
+            {
+                if (this.IsDone == value)
+                    return;
+                this.IsDone = value;
+                OnPropertyChanged("isDone");
+            }
+        }
+        public string text
+        {
+            get { return this.Text; }
+            set
+            {
+
+                if (this.Text == value)
+                    return;
+                this.Text = value;
+                OnPropertyChanged("text");
+            }
+        }
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
